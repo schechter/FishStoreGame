490781b163ec3c77f9c6ab6aabccc970eaaccf11
Mike Schechter <schechtermike@gmail.com>
2013-07-08 23:00:49 -0400

    verifies presences of user name for user @dmaaron

--- FishStoreApp/app/models/species.rb
+++ FishStoreApp/app/models/species.rb
@@ -3,4 +3,6 @@ class Species < ActiveRecord::Base
 
 validates_presence_of :appetite, :cleanliness_level, :image, :name, :price
 validates_numericality_of :appetite, :cleanliness_level, :price
+
+has_many :fishes
 end
--- FishStoreApp/spec/models/user_spec.rb
+++ FishStoreApp/spec/models/user_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe User do
-  pending 
+  describe 'validations' do
+  	it {should validate_presence_of(:user_name) }
+  end
 end
