fbd3cc78b5a8a5806fbfd15403174379f5474676
Mike Schechter <schechtermike@gmail.com>
2013-07-09 22:49:01 -0400

    maybe fixed weird heroku problem? @dmaaron

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ FishStoreApp::Application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => 'users/omniauth_callbacks' }
 
 root :to => "home#index"
-resources :species, only: [:new]
+
 resources :fish, only: [:new, :create, :destroy]
 end
