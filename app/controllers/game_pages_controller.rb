ec3100bdd4a9cb65c284b6bb6466b20e54a55352
Mike Schechter <schechtermike@gmail.com>
2013-07-11 08:58:36 -0400

    new game page shows uniquily sized unowned aquaria, game play page shows aquaria owned by current user @dmaaron

--- app/controllers/game_pages_controller.rb
+++ app/controllers/game_pages_controller.rb
@@ -4,12 +4,14 @@ class GamePagesController < ApplicationController
   end
 
   def new
+    @aquaria = Aquarium.unique_unowned_aquaria  #this method should be cleaned up?? Should be one sql querry but works
   end
 
   def play
     @species = Species.all
     @fish = Fish.all
-    @aquaria = Aquarium.unique_unowned_aquaria  #this method should be cleaned up?? Should be one sql querry but works
+    @aquaria = Aquarium.where(user_id: current_user)
+    
   end
 
   def paypal
