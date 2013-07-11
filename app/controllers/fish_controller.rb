a55b67a2f2bb1cf90ca465b6a4857b95a8ceb7b8
Mike Schechter <schechtermike@gmail.com>
2013-07-10 21:49:52 -0400

    when user buys a fish their funds are decreased @dmaaron

--- app/controllers/fish_controller.rb
+++ app/controllers/fish_controller.rb
@@ -11,6 +11,7 @@ class FishController < ApplicationController
     roll = rand(0.8..1.2)
     fish = Fish.new(name: Faker::Name.first_name, species_id: species.id, adjusted_appetite: species.appetite*roll, adjusted_cleanliness: species.cleanliness_level*roll)
     fish.save!
+    current_user.decrease_funds(species.price)
     redirect_to :back
   end
 
@@ -21,7 +22,7 @@ class FishController < ApplicationController
 
   def sell_me
     fish = Fish.find(params[:id])
-    current_user.increase_funds(fish.species.price)
+    current_user.increase_funds(fish.species.price*1.5)
     fish.destroy
     redirect_to :back  #need to ajax something here
   end
