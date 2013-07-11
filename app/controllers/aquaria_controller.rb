73e5b67959d7c5b302ff5085ea66d5e070fc4585
David Aaron <davidmatthewaaron@gmail.com>
2013-07-11 10:40:46 -0400

    working on purchase restrictions for aquaria fish based on user funds

--- app/controllers/aquaria_controller.rb
+++ app/controllers/aquaria_controller.rb
@@ -1,10 +1,14 @@
 class AquariaController < ApplicationController
 
-def sell_me
-  aquarium = Aquarium.find(params[:id])
-  current_user.decrease_funds(aquarium.price)
-  current_user.aquaria << aquarium
-  redirect_to :back
-end
-
+  def sell_me
+    aquarium = Aquarium.find(params[:id])
+    if current_user.funds >= aquarium.price
+      unless current_user.aquaria.count == 6
+        current_user.decrease_funds(aquarium.price)
+        current_user.aquaria << aquarium
+        p current_user.aquaria.count
+      end
+    end
+    redirect_to :back
+  end
 end
--- app/controllers/fish_controller.rb
+++ app/controllers/fish_controller.rb
@@ -23,17 +23,17 @@ class FishController < ApplicationController
   def sell_me
     fish = Fish.find(params[:id])
     current_user.decrease_funds(fish.species.price)
-    fish.destroy
     redirect_to :back  #need to ajax something here
   end
 
 
   def buy_me
     fish = Fish.find(params[:id])
-    current_user.increase_funds(fish.species.price*1.5)
-    fish.destroy
-    redirect_to :back  #need to ajax something here
+    unless current_user.funds >= fish.species.price
+      current_user.increase_funds(fish.species.price*1.5)
+      fish.destroy
+      redirect_to :back  #need to ajax something here
+    end
   end
-
 
 end
