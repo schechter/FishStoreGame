b043bec81291cf5d1732c582cce85273fff5c632
Mike Schechter <schechtermike@gmail.com>
2013-07-11 12:01:28 -0400

    user can buy and aquarium and it is added to the aquarium holder

--- app/controllers/game_pages_controller.rb
+++ app/controllers/game_pages_controller.rb
@@ -6,12 +6,15 @@ class GamePagesController < ApplicationController
   def new
     @fish = Fish.all
     @aquaria = Aquarium.unique_unowned_aquaria  #this method should be cleaned up?? Should be one sql querry but works
+    @users_aquaria = Aquarium.where(user_id: current_user.id)
+    p '========================'
+    p @users_aquaria
   end
 
   def play
     @species = Species.all
     @fish = Fish.all
-    @aquaria = Aquarium.where(user_id: current_user)
+    @aquaria = Aquarium.where(user_id: current_user.id)
     
   end
 
