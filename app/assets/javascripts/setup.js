e894de01f10cec9d4e4ad7309a58ea58d563f48c
Mike Schechter <schechtermike@gmail.com>
2013-07-11 21:08:35 -0400

    add divs for fish i aquarium @dmaaron

--- app/assets/javascripts/setup.js
+++ app/assets/javascripts/setup.js
@@ -1,8 +1,7 @@
 $('.fish-div').draggable( {revert:true});
   $('.droppable').droppable({
     drop: function( event, ui ) {
-    console.log(event);
-    console.log(ui);
-    console.log(event.target);
+    console.log($(event.target).find('p').text());
+    console.log(ui.draggable.context.outerText);
   }
 });
