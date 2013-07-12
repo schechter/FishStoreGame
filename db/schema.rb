c7e88252c4488b4814747833f28da6e0b2429a50
Mike Schechter <schechtermike@gmail.com>
2013-07-12 10:04:52 -0400

    email confirmation sending and confirming

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130709151643) do
+ActiveRecord::Schema.define(:version => 20130712134447) do
 
   create_table "aquaria", :force => true do |t|
     t.integer  "fish_capacity"
@@ -71,6 +71,10 @@ ActiveRecord::Schema.define(:version => 20130709151643) do
     t.datetime "updated_at",                             :null => false
     t.float    "funds"
     t.string   "user_name"
+    t.string   "confirmation_token"
+    t.string   "unconfirmed_email"
+    t.string   "confirmed_at"
+    t.string   "confirmation_sent_at"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
