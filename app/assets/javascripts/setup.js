49fd0fdeb6b91959a404b0c1ac0ff381afec9152
Mike Schechter <schechtermike@gmail.com>
2013-07-11 20:44:59 -0400

    hides aquaria id in hidden p tag @dmaaron

--- app/assets/javascripts/setup.js
+++ app/assets/javascripts/setup.js
@@ -1,9 +1,8 @@
 $('.fish-div').draggable( {revert:true});
   $('.droppable').droppable({
     drop: function( event, ui ) {
-    console.log('Fish Dropped')
     console.log(event);
     console.log(ui);
-    console.log(event.target)
+    console.log(event.target);
   }
 });
