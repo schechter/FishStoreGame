07203f56caee482a0a23cf8d55c3ba54a2b92749
David Aaron <davidmatthewaaron@gmail.com>
2013-07-12 00:47:10 -0400

    removing setup.js

--- app/assets/javascripts/play.js
+++ app/assets/javascripts/play.js
@@ -18,10 +18,13 @@ function make_food() {
     }
 }
 
+function food_ready(){
+    $('#food-maker').draggable({revert: true});
+}
+
 function make_water() {
     water_timer = setInterval(add_water, 1);
     function add_water() {
-        console.log('making water');
         var water = $('<div>');
         water.addClass('water');
         water.css('background-color', water_color());
@@ -35,14 +38,9 @@ function make_water() {
 }
 
 function water_ready(){
-    console.log('food ready!');
     $('#water-maker').draggable({revert: true});
 }
 
-function food_ready(){
-    console.log('food ready!');
-    $('#food-maker').draggable({revert: true});
-}
 
 function food_color() {
     var color = "rgb(" + _.random(0, 255) + "," + _.random(0, 255) + "," + _.random(0, 255) + ")";
@@ -50,19 +48,28 @@ function food_color() {
 }
 
 function water_color(){
-var color = "rgb(" + _.random(0, 70) + "," + _.random(0, 255) + ","+ _.random(180, 255)+")";
-return color;
+    var color = "rgb(" + _.random(0, 70) + "," + _.random(0, 255) + ","+ _.random(180, 255)+")";
+    return color;
 }
 
+
+$('.fish-div').draggable( {revert:true});
+
+
 $('.droppable').droppable({
-    drop: function(event, ui) {
-        console.log('Food Dropped');
-        console.log(event);
-        console.log(ui);
-        console.log(event.target);
-    }
+    drop: function( event, ui ) {
+    console.log(event);
+    console.log($(event.target).find('p').text());
+    console.log(ui.draggable.context.outerText);
+    console.log($(event.target).find('span').text());
+    // add_fish_to_aquarium();
+  }
 });
 
+
+function add_fish_to_aquarium(){
+  console.log('we made it to fish town');
+}
 
 
 $(function() {
--- app/assets/javascripts/setup.js
+++
@@ -1,7 +0,0 @@
-$('.fish-div').draggable( {revert:true});
-  $('.droppable').droppable({
-    drop: function( event, ui ) {
-    console.log($(event.target).find('p').text());
-    console.log(ui.draggable.context.outerText);
-  }
-});
