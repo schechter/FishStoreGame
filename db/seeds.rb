55e32bff8f436e0c571536032c5ff59e55d1fc22
David Aaron <davidmatthewaaron@gmail.com>
2013-07-14 21:20:12 -0400

    adjusting size of supply store and users aquarium

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,6 @@ User.delete_all
 
 s1 = Species.create(price: 1, image: 'http://www.aquarienfisch.net/wp-content/uploads/2010/09/Guppy.jpg', name: 'Guppy', appetite: 150, cleanliness_level: 100)
 s2 = Species.create(price: 1, image: 'http://2.bp.blogspot.com/_ckBlasgNSzg/SHKo412KgMI/AAAAAAAAGxQ/81oe3eovg8A/s400/rope+fish.jpg', name: 'Reedfish', appetite: 100, cleanliness_level: 90)
-s3 = Species.create(price: 3, image: 'http://2.bp.blogspot.com/-cVT9gB7AFJ8/USdKpqUxHqI/AAAAAAAACgU/cKhRXKLt2PI/s1600/goldfish.jpg', name: 'Gold Fish', appetite: 95, cleanliness_level: 90)
 s4 = Species.create(price: 4, image: 'http://www.dnr.sc.gov/fish/species/assets/longnosegar.jpg', name: 'Longnose gar', appetite: 80, cleanliness_level: 80)
 s5 = Species.create(price: 6, image: 'http://i16.photobucket.com/albums/b29/almeza/Pterophyllum_altum1.jpg', name: 'Angel Fish', appetite: 40, cleanliness_level: 70)
 s6 = Species.create(price: 6, image: 'http://images04.olx.in/ui/10/01/61/1292324332_126830061_1-Pictures-of--spotted-sailfin-pelco-sucker-fish-for-sale-1292324332.jpg', name: 'Sucker Fish', appetite: 1, cleanliness_level: 150)
